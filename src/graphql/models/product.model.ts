4175cc1ec650799b2d3e2cc769591e443bed4fcf
Irek <gos3r78@gmail.com>
2024-03-12 23:50:45 +0100

    Add default value for price field in Product model

--- src/graphql/models/product.model.ts
+++ src/graphql/models/product.model.ts
@@ -23,6 +23,7 @@ export class Product extends BaseEntity {
 	@Column()
 	@Field({ description: 'Name of the product' })
 	name: string;
+
 	@Column()
 	@Field({
 		nullable: true,
@@ -30,11 +31,16 @@ export class Product extends BaseEntity {
 	})
 	description: string;
 	@Column({ type: 'double precision' })
-	@Field(() => Float, { description: 'Price of the product' })
+	@Field(() => Float, {
+		defaultValue: 0,
+		description: 'Price of the product',
+	})
 	price: number;
+
 	@Column()
 	@Field({ description: 'Image of the product' })
 	product_image: string;
+
 	@Column()
 	@Field({ description: 'Slug of the product' })
 	slug: string;
