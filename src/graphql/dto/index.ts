9b8c09193b66ffb80b7bc1faa615d4e254da3a83
IrePro78 <gos3r78@gmail.com>
2024-03-22 14:59:38 +0100

    Add GraphQL DTO and models

---
+++ src/graphql/dto/index.ts
@@ -0,0 +1,4 @@
+export * from './create-order-item.input';
+export * from './create-order.input';
+export * from './create-product.input';
+export * from './update-order-item.input';
---
+++ src/graphql/models/index.ts
@@ -0,0 +1,6 @@
+export * from './product.model';
+export * from './category.model';
+export * from './collection.model';
+export * from './order.model';
+export * from './order-items.model';
+export * from './review.model';
